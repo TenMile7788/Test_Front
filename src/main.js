cf8460e5419fa9625d3f3572497737a54254c2c4
TenMile7788 <jihwan7788@gmail.com>
2024-05-30 10:23:47 +0900

    remove

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from "@/scripts/router"
-import store from "@/scripts/store"
-import VueCookies from "vue-cookies";
-
-createApp(App).use(router).use(store).use(VueCookies).mount('#app')
